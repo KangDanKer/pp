5f81b0adf56a96f14c954dc111d8c2dd4bf06477
KangDanKer <78604911+KangDanKer@users.noreply.github.com>
2024-08-18 22:01:07 +0700

    Create pp.sh

---
+++ pp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BLUE='\033[1;34m'
+NC='\033[0m'
+
+echo -e "${BLUE}"
+echo "KANGDANKER"
+echo ""
+echo "Follow Telegram : @KANGDANKER"
+echo "Website :https://KANGDANKER.xyz"
+echo -e "${NC}"
